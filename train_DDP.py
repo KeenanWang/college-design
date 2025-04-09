bfa99163e827e57434161cb992b2511a9c626da0
KeenanWang <1578439463@qq.com>
2025-04-09 13:35:06 +0800

    改掉softmax成sigmoid，第六次训练

--- train_DDP.py
+++ train_DDP.py
@@ -134,50 +134,44 @@ if __name__ == "__main__":
 
                 # TensorBoard日志记录
                 if global_step % 100 == 0:
-                    for name in loss_stats_rgb:
-                        dist.all_reduce(loss_stats_rgb[name], op=dist.ReduceOp.SUM)
-                        writer.add_scalar(f"RGBLoss/{name}", loss_stats_rgb[name].mean() / dist.get_world_size(),
-                                          global_step)
-                    for name in loss_stats_thermal:
-                        dist.all_reduce(loss_stats_thermal[name], op=dist.ReduceOp.SUM)
-                        writer.add_scalar(f"ThermalLoss/{name}", loss_stats_thermal[name].mean() / dist.get_world_size(),
-                                          global_step)
-                    for name in loss_stats_output:
-                        dist.all_reduce(loss_stats_output[name], op=dist.ReduceOp.SUM)
-                        writer.add_scalar(f"OutputLoss/{name}", loss_stats_output[name].mean() / dist.get_world_size(),
-                                          global_step)
-                    writer.add_scalar("Global_Loss", global_loss, global_step)
-                    writer.add_scalar("Params/lr", optimizer.param_groups[0]['lr'], global_step)
+                    for name, value in loss_stats_rgb.items():
+                        writer.add_scalar(f"RGBLoss/{name}", value.mean(), global_step)
+                    for name, value in loss_stats_thermal.items():
+                        writer.add_scalar(f"ThermalLoss/{name}", value.mean(), global_step)
+                    for name, value in loss_stats_output.items():
+                        writer.add_scalar(f"OutputLoss/{name}", value.mean(), global_step)
+                        writer.add_scalar("Global_Loss", global_loss, global_step)
+                        writer.add_scalar("Params/lr", optimizer.param_groups[0]['lr'], global_step)
 
-                # 更新进度条信息
-                pbar.set_postfix({
-                    'loss': f"{global_loss:.4f}",
-                    'lr': f"{optimizer.param_groups[0]['lr']:.2e}",
-                    'step': global_step
-                })
-            epoch_loss_total += global_loss
-            global_step += 1
+                    # 更新进度条信息
+                    pbar.set_postfix({
+                        'loss': f"{global_loss:.4f}",
+                        'lr': f"{optimizer.param_groups[0]['lr']:.2e}",
+                        'step': global_step
+                    })
+                epoch_loss_total += global_loss
+                global_step += 1
 
-        if local_rank == 0:
-            epoch_loss_total /= len(data_loader)  # 计算这一个epoch的平均损失
-            if epoch_loss_total < loss_min:
-                loss_min = epoch_loss_total
+            if local_rank == 0:
+                epoch_loss_total /= len(data_loader)  # 计算这一个epoch的平均损失
+                if epoch_loss_total < loss_min:
+                    loss_min = epoch_loss_total
+                    save_model(model=model.module,  # 注意获取原始模型
+                               save_path=f'runs/best_model.pth',
+                               epoch=epoch,
+                               optimizer=optimizer, global_step=global_step, loss_min=loss_min)
+                    print(f"最佳模型为{epoch}，其损失为{loss_min}")
                 save_model(model=model.module,  # 注意获取原始模型
-                           save_path=f'runs/best_model.pth',
+                           save_path=f'runs/last.pth',
                            epoch=epoch,
                            optimizer=optimizer, global_step=global_step, loss_min=loss_min)
-                print(f"最佳模型为{epoch}，其损失为{loss_min}")
-            save_model(model=model.module,  # 注意获取原始模型
-                       save_path=f'runs/last.pth',
-                       epoch=epoch,
-                       optimizer=optimizer, global_step=global_step, loss_min=loss_min)
-            pbar.close()  # 关闭当前epoch的进度条
+                pbar.close()  # 关闭当前epoch的进度条
 
-        if epoch in opt.lr_step:
-            lr = opt.lr * (0.1 ** (opt.lr_step.index(epoch) + 1))
-            print('Drop LR to', lr)
-            for param_group in optimizer.param_groups:
-                param_group['lr'] = lr
-    if local_rank == 0:
-        writer.close()
-    dist.destroy_process_group()
+            if epoch in opt.lr_step:
+                lr = opt.lr * (0.1 ** (opt.lr_step.index(epoch) + 1))
+                print('Drop LR to', lr)
+                for param_group in optimizer.param_groups:
+                    param_group['lr'] = lr
+        if local_rank == 0:
+            writer.close()
+        dist.destroy_process_group()
